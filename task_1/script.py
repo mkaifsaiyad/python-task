0b5efffd9c94548ed3e9bd47bffecc038fe31b00
mkaifsaiyad <kaif.saiyad@techholding.co>
2025-02-27 23:47:24 +0530

    fix: task-1 task-4

--- task_1/script.py
+++ task_1/script.py
@@ -3,12 +3,15 @@ import subprocess
 import sys
 
 def install(n, v):
-    subprocess.call([sys.executable, '-m', 'pip', 'install', f'{n}=={v}'])
-    import requests
-    if requests.__version__ != v:
-        print('\nVersion missmatch')
-    else:
-        print(f"\nmodule {n} version {v} installed and imported successfully!")
+    try:
+        subprocess.call([sys.executable, '-m', 'pip', 'install', f'{n}=={v}'])
+        import requests
+        if requests.__version__ != v:
+            print('\nVersion missmatch')
+        else:
+            print(f"\nmodule {n} version {v} installed and imported successfully!")
+    except:
+        print('some want wrong')
 
 # def install_and_check(package):
 #     pip.main(['install', package])
